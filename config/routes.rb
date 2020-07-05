a878b58f094642fbbf5441ecfe1999f0c5d9d9cb
BenDunjay <bendunjay@outlook.com>
2020-07-05 17:37:29 +0100

    redirect to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails
-  resources :alcohols, only: [:index, :show] 
-  resources :garnishes, only: [:index, :show] 
-  resources :mixers, only: [:index, :show] 
+  resources :alcohols, only: [:index, :show]
+  resources :garnishes, only: [:index, :show]
+  resources :mixers, only: [:index, :show]
   resources :users
-  resources :cocktails, only: [:index, :show, :new, :create, :edit, :update, :destroy] 
-    # login functionality - sessions
-    get "signup", to: "users#new", as: "signup"
-    get "login", to: "sessions#new", as: "login"
-    post "sessions", to: "sessions#create", as: "sessions"
-    delete "sessions", to: "sessions#destroy"
+  resources :cocktails, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  # login functionality - sessions
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  post "sessions", to: "sessions#create", as: "sessions"
+  delete "sessions", to: "sessions#destroy"
+  get "/", to: "sessions#new"
 end
